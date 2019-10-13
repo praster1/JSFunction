4d80fb63f422be66fa37d76e096ce8fd8393564a
JaeseongYoo <praster1@gmail.com>
2019-10-13 14:41:11 +0900

    big_letters, list_to_matrix

---
+++ big_letters.R
@@ -0,0 +1,12 @@
+require(compiler)
+
+big_letters = function(len) 
+{
+	res = list(); counter = 0
+	pasteFunc = function(j, i) { if (i == 1) { return(letters[j]); } else { return(paste(letters[i-1], letters[j], sep="")) } }
+	loopFunc = function(i) { 	res = lapply(1:26, pasteFunc, i); 	return(res) }
+	
+	res = unlist(pblapply(1:ceiling(len/26), loopFunc))
+	return(res[1:len])
+}
+big_letters = cmpfun(big_letters)
