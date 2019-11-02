f003b7c9e4b07041bc4d42b51895862ac05b5d99
JaeseongYoo <praster1@gmail.com>
2019-11-03 03:06:30 +0900

    Create returnColorList.R

---
+++ R/returnColorList.R
@@ -0,0 +1,28 @@
+### Color List
+returnColorList = function(n = NULL)
+{
+	colorNames = rownames(brewer.pal.info)
+	findColorList = function(i)
+	{
+		return(brewer.pal(n=8, name=colorNames[i]))
+	}
+
+	matColorList = list_to_matrix(lapply(1:length(colorNames), findColorList), colNum=8)
+	resColorList = NULL
+	for (i in 8:1)
+	{
+		resColorList = c(resColorList, matColorList[,i])
+	}
+	
+	if (is.null(n)) {
+		n = length(resColorList)
+	} else {
+		while(length(resColorList) < n)
+		{
+			resColorList = c(resColorList, resColorList)
+		}
+	}
+	
+	
+	return(resColorList[1:n])
+}
