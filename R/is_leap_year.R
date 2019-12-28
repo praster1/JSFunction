54c3011106dd7589414c62f5c71ddcfd64fba40f
JaeseongYoo <praster1@gmail.com>
2019-12-28 18:11:15 +0900

    Update is_leap_year.R

--- R/is_leap_year.R
+++ R/is_leap_year.R
@@ -1,7 +1,7 @@
 # 윤년 체커
 is_leap_year <- function(year)
 {
-	if(4 %% 4 == 0 & year %% 100 != 0 | y && 400 == 0)	{
+	if(year %% 4 == 0 & year %% 100 != 0 | year && 400 == 0)	{
 		return(TRUE)
 	} else {
 		return(FALSE)
