fa081f15e3230627849f2a766136a8ed3e825228
JaeseongYoo <praster1@gmail.com>
2020-10-02 09:42:25 +0900

    Update getYearDates.R

--- R/getYearDates.R
+++ R/getYearDates.R
@@ -1,11 +1,15 @@
 ##### 날짜를 character YYYY-MM-DD HH:mm:ss 에서 numeric YYYYMMDD로 변환
 getYearDates = function(yearDateVec, limit="month")	
 {
-	res = NULL;	colName = NULL;
-	if			(limit == "year")		{		res = substr(as.character(yearDateVec), 1, 4);	colName = "Years"}
-	else if	(limit == "month")		{		res = substr(as.character(yearDateVec), 1, 7);	colName = "YearMonth"}
-	else									{		res = substr(as.character(yearDateVec), 1, 10);	colName = "YearDates"}
-	res = gsub("-", "", res);	res = data.table(res);		colnames(res) = colName
+	res = colName = NULL;
+	if			(limit == "year")		{		res = substr(yearDateVec, 1, 4);	colName = "Years"}
+	else if	(limit == "month")		{		res = substr(yearDateVec, 1, 7);	colName = "YearMonth"}
+	else									{		res = substr(yearDateVec, 1, 10);	colName = "YearDates"}
+
+	res = gsub("-", "", res);
+	res = data.table(res);
+	colnames(res) = colName
+
 	return(res)	
 }
 getYearDates = cmpfun(getYearDates)
